91faf90db947d2701418dba879b6c52db41936cc
akaleshtanwer <akalesh.aurasoft@gmail.com>
2023-12-07 22:53:15 +0530

    optimised payment ccode

--- Harshkala_naitik/src/controller/Payment/PaymentController.js
+++ Harshkala_naitik/src/controller/Payment/PaymentController.js
@@ -1,36 +1,42 @@
 const Razorpay = require("razorpay");
 const crypto = require('crypto');
-const KEY_ID = 'rzp_test_QjwunCBCAXXLPv'
-const KEY_SECRET = 'uQWsLENJXKZc2C6YiLSjzWB6'
 
-exports.checkoutPayment = async(req,res)=>{
-    let instance = new Razorpay({ key_id: KEY_ID, key_secret: KEY_SECRET })
+const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_ID,
+  key_secret: process.env.RAZORPAY_SECRET
+});
 
-    var options = {
-        amount: req.body.amount * 100, 
-        currency: "INR",
+exports.checkoutPayment = async (req, res) => {
+  try {
+    const options = {
+      amount: req.body.amount * 100,
+      currency: "INR",
     };
 
-    instance.orders.create(options, function (err, order) {
-        if (err) {
-            return res.send({ code: 500, message: 'Server Err.' })
-        }
-        return res.send({ code: 200, message: 'order created', data: order })
-    });
-}
+    const order = await razorpay.orders.create(options);
+    res.status(200).json({ code: 200, message: 'Order created', data: order });
+  } catch (err) {
+    console.error('Error creating order:', err);
+    res.status(500).json({ code: 500, message: 'Server Error' });
+  }
+};
 
-exports.verifyPayment = async (req,res)=>{
+exports.verifyPayment = async (req, res) => {
+  try {
+    const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body.response;
+    const body = `${razorpay_order_id}|${razorpay_payment_id}`;
 
-    let body = req.body.response.razorpay_order_id + "|" + req.body.response.razorpay_payment_id;
+    const expectedSignature = crypto.createHmac('sha256', process.env.RAZORPAY_SECRET)
+      .update(body)
+      .digest('hex');
 
-    var expectedSignature = crypto.createHmac('sha256', KEY_SECRET)
-        .update(body.toString())
-        .digest('hex');
-
-    if (expectedSignature === req.body.response.razorpay_signature) {
-        res.send({ code: 200, message: 'Sign Valid' });
+    if (expectedSignature === razorpay_signature) {
+      res.status(200).json({ code: 200, message: 'Sign Valid' });
     } else {
-
-        res.send({ code: 500, message: 'Sign Invalid' });
+      res.status(500).json({ code: 500, message: 'Sign Invalid' });
     }
-}
+  } catch (err) {
+    console.error('Error verifying payment:', err);
+    res.status(500).json({ code: 500, message: 'Server Error' });
+  }
+};
